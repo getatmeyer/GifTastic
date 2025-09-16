f863a71fd58d03585db45affc5e4092c66ce15ba
Nathan M <get.at.meyer@gmail.com>
2025-09-16 01:05:53 -0400

    lets see

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -35,10 +35,12 @@
     event.preventDefault(); // prevents the form from trying to sumbit itself. Instead, the user can hit enter
 
     var marvel = $("#marvel-input").val().trim();
+    if (marvel.length > 0) {
     characters.push(marvel);
     console.log("push"); // PASSED
 
     marvelButtons();
+    }
 
   });
   marvelButtons();
@@ -59,11 +61,15 @@
       url: queryURL,
       method: "GET"
     })
+
+
       // After the data comes back from the API
       .then(function (response) {
 
         // Storing an array of results in the results variable
         var results = response.data;
+
+       
         // console.log(results);
         console.log(results);
 
@@ -131,6 +137,7 @@
             });
 
         })
+        
       })
 
 
